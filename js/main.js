d4ab7d8368341ed3a0dabfd717a168e56cb94bb9
Gabriel Adriano <103237064+Gabrielfull@users.noreply.github.com>
2022-06-30 21:16:46 -0300

    ajeitando IMC

--- js/main.js
+++ js/main.js
@@ -15,31 +15,31 @@ function meuForm () {
             altura: altura.value,
     });
 
-    console.log(dados)
+    const imc = peso.value / altura.value ** 2;
 
-    if (peso.value / (altura.value * altura.value) > 0 && peso.value / (altura.value * altura.value) < 18.5){
+    if (imc > 0 && imc < 18.5){
 
-        resultado.innerHTML = `<p style="background-color: #F7E318;">Abaixo do peso</p>`
+        resultado.innerHTML = `<p style="background-color: #F7E318;">Seu Imc é ${imc.toFixed(1)} <strong>(Abaixo do peso)</strong></p>`
 
-    } else if (peso.value / (altura.value * altura.value) > 18.6 && peso.value / (altura.value * altura.value) < 24.9){
-
-        resultado.innerHTML = `<p>Peso normal</p>`
-
-    }else if (peso.value / (altura.value * altura.value) > 25 && peso.value / (altura.value * altura.value) < 29.9){
-
-        resultado.innerHTML = `<p>Sobre peso</p>`
+    } else if (imc > 18.6 && imc < 24.9){
         
-    }else if (peso.value / (altura.value * altura.value) > 30 && peso.value / (altura.value * altura.value) < 34.9) {
+        resultado.innerHTML = `<p>Seu Imc é ${imc.toFixed(1)} <strong>(Peso normal)</strong></p>`
 
-        resultado.innerHTML = `<p style="background-color: brown;">Obesidade grau 1</p>`
+    }else if (imc > 25 && imc < 29.9){
 
-    }else if (peso.value / (altura.value * altura.value) > 35 && peso.value / (altura.value * altura.value) < 39.9) {
+        resultado.innerHTML = `<p>Seu Imc é ${imc.toFixed(1)}<strong> (Sobre peso)</strong></p>`
+        
+    }else if (imc > 30 && imc < 34.9) {
 
-        resultado.innerHTML = `<p style="background-color: brown;">Obesidade grau 2<strong>(CUIDADO)</strong></p>`
+        resultado.innerHTML = `<p style="background-color: brown;">Seu Imc é ${imc.toFixed(1)} <strong>(Obesidade grau 1)</strong</p>`
 
-    }else if (peso.value / (altura.value * altura.value) > 40) {
+    }else if (imc > 35 && imc < 39.9) {
 
-        resultado.innerHTML = `<p style="background-color: brown;">Obesidade grau 3 <strong>(URGENTE)</strong></p>`
+        resultado.innerHTML = `<p style="background-color: brown;">Seu Imc é ${imc.toFixed(1)} <strong>(Obesidade grau 2)</strong</p>`
+
+    }else if (imc > 40) {
+
+        resultado.innerHTML = `<p style="background-color: brown;">Seu Imc é ${imc.toFixed(1)} <strong>(Obesidade grau 3)</strong></p>`
 
     }else {
         resultado.innerHTML = `<p style="background-color: red;">Valor invalido</p>`
